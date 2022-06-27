9497292996d69c289032b7c10ba91b6cb8561647
alexey <fant205@mail.ru>
2022-06-27 12:47:18 +0300

    fix

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -28,18 +28,7 @@ select * from _countries;
 	--удалить колонки
 	ALTER TABLE _countries 
 		DROP COLUMN title_en,
-		DROP COLUMN title_ua,
-		DROP COLUMN title_be,
-		DROP COLUMN title_es,
-		DROP COLUMN title_pt,
-		DROP COLUMN title_de,
-		DROP COLUMN title_fr,
-		DROP COLUMN title_it,
-		DROP COLUMN title_pl,
-		DROP COLUMN title_ja,
-		DROP COLUMN title_lt,
-		DROP COLUMN title_lv,
-		DROP COLUMN title_cz;
+		
 
 
 	--изменение поля title_ru
@@ -55,10 +44,6 @@ select * from _countries;
 		--индекс 		
 			create index idx_countries_title on _countries (title);
 
-	--изменение поля coutry_id
-		--переименование и NOT NULL
-			ALTER TABLE _countries RENAME COLUMN country_id to id;			
-
 		--Для Postgres, что бы сделать автоинкремент пришлось поискать в интернете
 			CREATE SEQUENCE _countries_seq;
 			ALTER TABLE _countries ALTER COLUMN id SET DEFAULT nextval('_countries_seq');
@@ -68,158 +53,9 @@ select * from _countries;
 
 		-- PRIMARY KEY;
 			alter table _countries add primary key (id);
-
-		
-
---Таблица _regions
-	--удаление лишнего
-		ALTER TABLE _regions 
-			DROP COLUMN title_en,
-			DROP COLUMN title_ua,
-			DROP COLUMN title_be,
-			DROP COLUMN title_es,
-			DROP COLUMN title_pt,
-			DROP COLUMN title_de,
-			DROP COLUMN title_fr,
-			DROP COLUMN title_it,
-			DROP COLUMN title_pl,
-			DROP COLUMN title_ja,
-			DROP COLUMN title_lt,
-			DROP COLUMN title_lv,
-			DROP COLUMN title_cz;
-			
-	--Поле region_id	
-		--переименование
-			ALTER TABLE _regions RENAME COLUMN region_id to id;			
-
-		--Для Postgres, что бы сделать автоинкремент пришлось поискать в интернете
-			CREATE SEQUENCE _regions_seq;
-			ALTER TABLE _regions ALTER COLUMN id SET DEFAULT nextval('_regions_seq');
-			ALTER TABLE _regions ALTER COLUMN id SET NOT NULL;
-			ALTER SEQUENCE _regions_seq OWNED BY _regions.id;
-			SELECT setval('_regions_seq', (SELECT max(id) FROM _regions));
-
-		-- PRIMARY KEY;
-			alter table _regions add primary key (id);
-
-
-	--Поле title_ru
-		--переименовать колонку
-			ALTER TABLE _regions RENAME COLUMN title_ru TO title;
-			
-		--изменить тип колонки
-			ALTER TABLE _regions ALTER COLUMN title SET DATA TYPE VARCHAR(150);
-
-		--сделать NOT NULL
-			ALTER TABLE _regions ALTER COLUMN title SET NOT NULL;		
-			
-		--индекс 		
-			create index idx_regions_title on _regions (title);
-			
-			
-	--Поле country_id			
-		--NOT NULL						
-			ALTER TABLE _regions ALTER COLUMN country_id SET NOT NULL;
 			
 		-- FOREIGN KEY;
-			alter table _regions add foreign key (country_id) references _countries (id);
-			
-			
-			
-			
-			
---Таблица _cities
---удаление лишнего
-	ALTER TABLE _cities 
-		DROP COLUMN title_en,
-		DROP COLUMN title_ua,
-		DROP COLUMN title_be,
-		DROP COLUMN title_es,
-		DROP COLUMN title_pt,
-		DROP COLUMN title_de,
-		DROP COLUMN title_fr,
-		DROP COLUMN title_it,
-		DROP COLUMN title_pl,
-		DROP COLUMN title_ja,
-		DROP COLUMN title_lt,
-		DROP COLUMN title_lv,
-		DROP COLUMN title_cz;
-		
-	ALTER TABLE _cities 
-		DROP COLUMN area_ru,
-		DROP COLUMN area_en,
-		DROP COLUMN area_ua,
-		DROP COLUMN area_be,
-		DROP COLUMN area_es,
-		DROP COLUMN area_pt,
-		DROP COLUMN area_de,
-		DROP COLUMN area_fr,
-		DROP COLUMN area_it,
-		DROP COLUMN area_pl,
-		DROP COLUMN area_ja,
-		DROP COLUMN area_lt,
-		DROP COLUMN area_lv,
-		DROP COLUMN area_cz;
-							
-	ALTER TABLE _cities 
-		DROP COLUMN region_ru,
-		DROP COLUMN region_en,
-		DROP COLUMN region_ua,
-		DROP COLUMN region_be,
-		DROP COLUMN region_es,
-		DROP COLUMN region_pt,
-		DROP COLUMN region_de,
-		DROP COLUMN region_fr,
-		DROP COLUMN region_it,
-		DROP COLUMN region_pl,
-		DROP COLUMN region_ja,
-		DROP COLUMN region_lt,
-		DROP COLUMN region_lv,
-		DROP COLUMN region_cz;
-			
-			
---Поле city_id	
-	--переименование
-		ALTER TABLE _cities RENAME COLUMN city_id to id;			
-
-	--Для Postgres, что бы сделать автоинкремент пришлось поискать в интернете
-		CREATE SEQUENCE _cities_seq;
-		ALTER TABLE _cities ALTER COLUMN id SET DEFAULT nextval('_cities_seq');
-		ALTER TABLE _cities ALTER COLUMN id SET NOT NULL;
-		ALTER SEQUENCE _cities_seq OWNED BY _cities.id;
-		SELECT setval('_cities_seq', (SELECT max(id) FROM _cities));
-
-	-- PRIMARY KEY;
-		alter table _cities add primary key (id);
-
-
---Поле title_ru
-	--переименовать колонку
-		ALTER TABLE _cities RENAME COLUMN title_ru TO title;
-		
-	--изменить тип колонки
-		ALTER TABLE _cities ALTER COLUMN title SET DATA TYPE VARCHAR(150);
-
-	--сделать NOT NULL
-		ALTER TABLE _cities ALTER COLUMN title SET NOT NULL;		
-		
-	--индекс 		
-		create index idx_cities_title on _cities (title);
-		
-		drop index idx_cities_title;
-
-		
-			
-	--Поле country_id			
-		--NOT NULL						
-			ALTER TABLE _cities ALTER COLUMN country_id SET NOT NULL;
-			
-		-- FOREIGN KEY;
-			alter table _cities add foreign key (country_id) references _countries (id);
-			
-	--Поле region_id											
-		-- FOREIGN KEY;
-			alter table _cities add foreign key (region_id) references _regions (id);
+			alter table _regions add foreign key (country_id) references _countries (id);			
 
 
 
@@ -231,11 +67,14 @@ select * from _countries;
 	grant all privileges on database geodata to user_db;
 
 
+
 --Создание таблицы:
 	--Main table:
 		create table users (
-			id serial primary key, --serial типа говорящий об автонумерации
-			name text -- 
+			id serial primary key, --serial указывает автонумерацию
+		    login text NOT NULL UNIQUE,
+		    password text NOT NULL,
+		    nickname text NOT NULL UNIQUE	
 		);
 	--Зависимая:
 		create table likes (
@@ -254,6 +93,10 @@ select * from _countries;
 		add column obj_type integer, 	--Добавить колонку
 		add foreign key (obj_type)		--Добавить внешний ключ
 			references obj_types (id); 
+
+--Удаление элементов таблицы
+	Alter table users
+	drop constraint users_pkey;
 
 
 --Импорт данных:
@@ -287,9 +130,89 @@ select * from _countries;
 		where t2.from_id is null; -- с помощью этого where делаем вычитание одного множества из другого
 
 
+--Функуция
+	--есть входные и выходные параметры:
+		CREATE OR REPLACE FUNCTION  getManagerName(in f1 text, in f2 text, out f3 text)
+	    AS $$ 
+		
+			select 'return result'
+		
+		$$
+	    LANGUAGE SQL;
+
+		--test
+			SELECT * FROM getManagerName('f1', 'f2');
+
+	--функция ничего не возвращает:
+		CREATE OR REPLACE FUNCTION  shareFile(in fileName text, in filePath text, in ownerUser text, in targerUser text) 
+		RETURNS void
+		AS $$ 
+
+		    select 'geg'
+
+		$$
+		LANGUAGE SQL;
+
+		--test
+	    	SELECT * FROM shareFile('fileName', 'filePath', 'ownerUser', 'targerUser');
 
 
 
+--Хранимая процедура
+	CREATE OR REPLACE PROCEDURE shareFile2(
+	"fileName" text,
+	"filePath" text,
+	"ownerName" text,
+	"targetUserName" text)
+	LANGUAGE 'plpgsql'
+	AS $BODY$
+	Declare  
+	    ownerId int;
+	    targetUserId int;
+	    fileId int;
+	Begin
 
 
+	    --search owner id
+	    select id into ownerId from users where nickname = "ownerName";
+	    
+	    --search targerUser id
+	    select id into targetUserId from users where nickname = "targetUserName";
 
+	    --save file to table
+	    insert into files (file_name, file_path, owner_id)
+	        values ("fileName", "filePath", ownerId);
+	        
+	    --search file id
+	    select id into fileId from files where files.file_name = "fileName" and files.file_path = "filePath" and files.owner_id = ownerId;
+	        
+	    --save info to share table
+	    insert into files_share (file_id, target_user_id)
+	        values (fileId, targetUserId);
+	        
+	        
+	end
+	$BODY$;
+
+	--полномочия на вызов
+		ALTER PROCEDURE shareFile2(text, text, text, text) OWNER TO fnrtuqrj;
+	       
+	--пример вызова
+		CALL shareFile2('file4', 'file1Path2', 'test', 'test2')
+
+	--пример вызова в Java:
+		public static void main(String[] args) throws SQLException {
+        try (Connection connection1 = DriverManager.getConnection(
+                "jdbc:postgresql://tyke.db.elephantsql.com:5432/fnrtuqrj",
+                "fnrtuqrj",
+                "NLpOUejgCpyCN9POcNC7XlDtSK3h4Hw6")) {
+
+            String preparedSql = " call shareFile2(?,?,?,?)";
+            try (CallableStatement cstmt = connection1.prepareCall(preparedSql)) {
+                cstmt.setString(1, "fileName111");
+                cstmt.setString(2, "filePath111");
+                cstmt.setString(3, "test");
+                cstmt.setString(4, "test2");
+                cstmt.execute();
+            }
+        }
