2a4d6207dcce6c70765cb332cc99aa10f82fcf85
Lex <fant205@mail.ru>
2022-07-08 17:32:55 +0300

    fix

---
+++ Maven/TestWebMaven/src/main/java/ru/test/axel/rest/Foo.java
@@ -0,0 +1,27 @@
+package ru.test.axel.rest;
+
+public class Foo {
+    private int id;
+    private String name;
+
+    public Foo(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
