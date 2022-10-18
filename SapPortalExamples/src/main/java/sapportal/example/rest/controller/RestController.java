baf4528b12cbfe3d03dcae8bbdd4e2265b338ecb
Lex <fant205@mail.ru>
2022-10-18 12:05:18 +0300

    fix

--- SapPortalExamples/src/main/java/sapportal/example/rest/controller/RestController.java
+++ SapPortalExamples/src/main/java/sapportal/example/rest/controller/RestController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sapportal.example.rest.model.Guitar;
 
+/**
+ * На SAP Portal целевой адрес будет:
+ *  https://host:port/SimpleRestWithSpring/rest/find
+ */
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = "/rest")
 public class RestController {
